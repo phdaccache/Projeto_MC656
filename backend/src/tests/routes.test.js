9777c6c0da4566cf9f4ad6dc1088eaa7a8e51888
Lawrence Melo <lawrencefmm@gmail.com>
2024-04-17 17:29:25 -0300

    fix: test script

--- backend/src/tests/routes.test.js
+++ backend/src/tests/routes.test.js
@@ -1,9 +1,13 @@
 const request = require("supertest");
 const app = require("../app");
 
-describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
+describe("GET / response should be 200", () => {
+  it("should be 200", async () => {
+      return request(app)
+          .get("/")
+          .expect(200)
+          .then((res) => {
+              expect(res.statusCode).toBe(200);
+          })
   });
 });

4daf1d670e2beda46b324d2899fc2261ed082257
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-04-18 17:10:29 -0300

    Add createTables() before exporting

--- backend/src/lib/dbConnection.js
+++ backend/src/lib/dbConnection.js
@@ -10,5 +10,31 @@ const dbClient = new Client({
 
 dbClient.connect();
 
+const errorHandler = (err, res) => {
+    if (err) {
+        console.error(err);
+        // @TODO dbClient.end() (async)            
+        return;
+    }
+}
+
+const createTables = async () => {
+    const usersTable = `
+    DROP TABLE IF EXISTS users;
+    CREATE TABLE users (
+        email varchar,
+        firstName varchar,
+        lastName varchar,
+        age int
+    );
+    `;
+    await dbClient.query(usersTable, errorHandler);
+    console.log("Table users created");
+    // Create other tables here ...
+}
+
+// Create tables
+createTables();
+
 // Export the client
 module.exports = dbClient;

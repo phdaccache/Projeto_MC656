34f3a1a7c77ca28fc9efc3843ddd32c67f8faf3a
phdaccache <pedrodaccache@gmail.com>
2024-04-19 20:27:21 -0300

    fix indentation cadastro-de-usuarios

--- backend/src/tests/routes.test.js
+++ backend/src/tests/routes.test.js
@@ -2,12 +2,12 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET / response should be 200", () => {
-  it("should be 200", async () => {
-      return request(app)
-          .get("/")
-          .expect(200)
-          .then((res) => {
-              expect(res.statusCode).toBe(200);
-          })
-  });
+    it("should be 200", async () => {
+        return request(app)
+            .get("/")
+            .expect(200)
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+            })
+    });
 });

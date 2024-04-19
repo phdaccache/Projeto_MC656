e5d80658e87b3eced337931f4223555b685fe3ff
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-04-19 16:25:59 -0300

    Update routes.js to use the controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,16 @@
 const express = require("express");
+const UserController = require("./controllers/UserController");
 
 const routes = express.Router();
 
-const dbClient = require("./lib/dbConnection");
-
-routes.get("/list_users", async (req, res) => {    
-    const client = await dbClient.connect();
-    const query = `
-        SELECT * FROM users;
-    `;    
-    await client.query(query, async (err, resp) => {
-        if (err) {
-            console.error(err);
-            await client.release(true);
-            return res.send("Internal error").status(500);
-        }
-        console.log(resp.rows);                       
-        await client.release(true);
-        return res.send(resp.rows).status(200);
-    });    
-});
-
 routes.get("/", (req, res) => {
-    return res.send("Bem vindo a plataforma de olimpiadas!\n").status(200);
+    return res.json({ok : "Bem vindo a plataforma de olimpiadas!\n"}).status(200);
 })
+
+/*
+======================= USUÁRIOS =======================
+*/
+routes.get("/list_user", UserController.index);
+routes.post("/insert_user", UserController.store);
 
 module.exports = routes;

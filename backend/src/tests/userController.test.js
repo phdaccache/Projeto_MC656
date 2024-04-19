debff7e985723e976a6f94aaff8e19065a432c2c
phdaccache <pedrodaccache@gmail.com>
2024-04-19 19:56:16 -0300

    test 4 for insert_user

--- backend/src/tests/userController.test.js
+++ backend/src/tests/userController.test.js
@@ -87,4 +87,25 @@ describe("POST /insert_user responses", () => {
                 expect(res.body.ok).toBe("Invalid email");
             })
     });
+
+    it("shouldn't allow invalid phone numbers", async () => {
+        const newUser = {
+            name: "Test User",
+            birth_date: "2022-01-01",
+            email: "testuserphone@gmail.com",
+            school: "Test School",
+            gender: "Test gender",
+            phone_number: "5a24-907"
+        };
+
+        // Primeira
+        return request(app)
+        .post("/insert_user")
+        .send(newUser)
+        .expect(400)
+        .then((res) => {
+            expect(res.body).toHaveProperty('ok');
+            expect(res.body.ok).toBe("Invalid phone number");
+        });
+    });
 });

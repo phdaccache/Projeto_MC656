ce140be98c80a1729eafa5c07f447995b01ef2c7
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-04-19 15:05:13 -0300

    Adiciona rotas de olimpiadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,36 @@
 const express = require("express");
+const OlympiadController = require('./controllers/OlympiadController');
 
 const routes = express.Router();
 
+// Alterar para controller
 const dbClient = require("./lib/dbConnection");
-
-routes.get("/list_users", async (req, res) => {    
+routes.get("/list_users", async (req, res) => {
     const client = await dbClient.connect();
     const query = `
         SELECT * FROM users;
-    `;    
+    `;
     await client.query(query, async (err, resp) => {
         if (err) {
             console.error(err);
             await client.release(true);
             return res.send("Internal error").status(500);
         }
-        console.log(resp.rows);                       
+        console.log(resp.rows);
         await client.release(true);
         return res.send(resp.rows).status(200);
-    });    
+    });
 });
 
 routes.get("/", (req, res) => {
     return res.send("Bem vindo a plataforma de olimpiadas!\n").status(200);
-})
+});
+
+
+/*
+======================= OLIMPÍADAS =======================
+*/
+routes.get('/list_olympiad', OlympiadController.index);
+routes.post('/insert_olympiad/:name/:date_start/:date_end/:school/:description', OlympiadController.store);
 
 module.exports = routes;

2a5efd2b274eff9e3a938b3d5abaa5596535c88c
phdaccache <pedrodaccache@gmail.com>
2024-04-19 19:23:55 -0300

    test 1 for list_olympiad

---
+++ backend/src/tests/olympiadcontroller.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /list_olympiad responses", () => {
+    it("should be 200", async () => {
+        return request(app)
+                .get("/list_olympiad")
+                .expect(200)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200);
+                })
+    });
+});

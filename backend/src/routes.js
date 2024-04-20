9f164fa71bbe3ed709b7575a273eb843ef5eab15
Lawrence Melo <lawrencefmm@gmail.com>
2024-04-19 22:13:00 -0300

    Revert "Merge pull request #20 from phdaccache/feature/routes-update"
    This reverts commit 43197f9780c86b318272d3f92f9ff9ba119356aa, reversing
    changes made to 96d322e8c68d5acf72ea8f63191a4eff070ece6f.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,20 +5,19 @@ const routes = express.Router();
 const dbClient = require("./lib/dbConnection");
 
 routes.get("/list_users", async (req, res) => {    
-    const client = await dbClient.connect();
     const query = `
         SELECT * FROM users;
     `;    
+    const client = await dbClient.connect();
     await client.query(query, async (err, resp) => {
         if (err) {
             console.error(err);
-            await client.release(true);
             return res.send("Internal error").status(500);
         }
         console.log(resp.rows);                       
-        await client.release(true);
         return res.send(resp.rows).status(200);
     });    
+    await client.release(true);
 });
 
 routes.get("/", (req, res) => {

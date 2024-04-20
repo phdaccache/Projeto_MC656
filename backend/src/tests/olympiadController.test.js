fa4f5de2ccc9fe541c8b6dabd1e13eff344faba8
André <a231392@dac.unicamp.br>
2024-04-19 21:30:12 -0300

    Force DB init before running

--- backend/src/tests/olympiadController.test.js
+++ backend/src/tests/olympiadController.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
+const dbClient = require("../lib/dbConnection"); // force DB init before running
 
 describe("GET /list_olympiad responses", () => {
     it("should be 200", async () => {

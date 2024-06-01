3b6f1216199442efe79a2e26504aaec214e0718c
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-01 17:31:14 -0300

    refactor: adiciona classes para controllers e banco de dados

--- backend/src/controllers/OlympiadController.js
+++ backend/src/controllers/OlympiadController.js
@@ -1,8 +1,8 @@
-const dbClient = require("../lib/dbConnection");
+const DbClient = require("../lib/dbConnection");
 
-module.exports = {
-    async index(req, res) {
-        const client = await dbClient.connect();
+class OlympiadController {
+    static async index(req, res) {
+        const client = new DbClient();
 
         const query = `
             SELECT * FROM OLYMPIAD LIMIT 10;
@@ -10,18 +10,15 @@ module.exports = {
 
         try {
             const result = await client.query(query);
-            await client.release(true);
             return res.status(200).json(result.rows);
         } catch (error) {
             console.log(error)
-            await client.release(true);
             return res.status(500).json({ok : "Internal error"});
         }
+    }
 
-    },
-
-    async store(req, res) {
-        const client = await dbClient.connect();
+    static async store(req, res) {
+        const client = new DbClient();
 
         const {name, date_start, date_end, school, description} = req.body;
 
@@ -30,7 +27,6 @@ module.exports = {
             SELECT * FROM OLYMPIAD WHERE name = '${name}';
         `);
         if (userExists.rowCount > 0) {
-            await client.release(true);
             return res.status(400).json({ok : "Olimpíada já cadastrada."});
         }
 
@@ -40,14 +36,13 @@ module.exports = {
             VALUES ('${name}', '${date_start}', '${date_end}', '${school}', '${description}');
         `;
 
-
         try {
             await client.query(query);
-            await client.release(true);
             return res.status(200).json({ok: true});
         } catch (error) {
-            await client.release(true);
             return res.status(500).json({ok : "Internal error"});
         }
     }
-};
+}
+
+module.exports = OlympiadController;
--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,41 +1,38 @@
-const dbClient = require("../lib/dbConnection");
+const DbClient = require("../lib/dbConnection");
 
-module.exports = {
-    async index (req, res) {
-        // GET
-        const client = await dbClient.connect();
+class UserController {
+    static async index (req, res) {
+        const client = new DbClient();
+
         const query = `
         SELECT * FROM users;
         `;
+
         await client.query(query, async (err, resp) => {
             if (err) {
                 // console.error(err);
-                await client.release(true);
                 return res.status(500).json({ok : "Internal error"});
             }
             const user_list = resp.rows;
             // console.log(user_list);
-            await client.release(true);
             return res.status(200).json({ user_list });
         });
-    },
+    }
 
-    async store (req, res) {
-        const client = await dbClient.connect();
-        // POST
+    static async store (req, res) {
+        const client = new DbClient();
+
         const { name, birth_date, email, school, gender, phone_number } = req.body;
 
         // Check if email is valid
         const email_regex = /^\S+@\S+\.\S+$/;
         if (!email_regex.test(email)) {
-            await client.release(true);
             return res.status(400).json({ok : "Invalid email"});
         }
 
         // Check if phone number is valid
         const phone_number_regex = /^\d{5}-\d{4}$/;
         if (!phone_number_regex.test(phone_number)) {
-            await client.release(true);
             return res.status(400).json({ok : "Invalid phone number"});
         }
 
@@ -48,11 +45,9 @@ module.exports = {
         await client.query(query, async (err, resp) => {
             if (err) {
                 console.error(err);
-                await client.release(true);
                 return res.status(500).json({ok : "Internal error"});
             }
             if (resp.rows.length > 0) {
-                await client.release(true);
                 return res.status(400).json({ok : "User already exists"});
             }
 
@@ -65,12 +60,12 @@ module.exports = {
             await client.query(insert_query, async (err, resp) => {
                 if (err) {
                     console.error(err);
-                    await client.release(true);
                     return res.status(500).json({ok : "Internal error"});
                 }
-                await client.release(true);
                 return res.status(200).json({ok : "User created"});
             });
         });
-    },
-};
+    }
+}
+
+module.exports = UserController;

13bab6571b0b1fe9fcb58ca6bc8b0808305fd9f8
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-13 15:40:20 -0300

    test: schoolController DELETE test

--- backend/src/tests/schoolController.test.js
+++ backend/src/tests/schoolController.test.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
 const app = require("../app");
-const User = require("../models/User");
 
 describe("POST /school responses", () => {
   it("should insert a new school", async () => {
@@ -16,6 +15,22 @@ describe("POST /school responses", () => {
       .then((res) => {
         expect(res.body).toHaveProperty("ok");
         expect(res.body.ok).toBe("School created");
+      });
+  });
+});
+
+describe("DELETE /school/:id responses", () => {
+  it("should delete the newly added school", async () => {
+    const school = {
+      name: "Test School",
+    };
+
+    return request(app)
+      .delete("/school/" + school.name)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toHaveProperty("ok");
+        expect(res.body.ok).toBe("School deleted");
       });
   });
 });

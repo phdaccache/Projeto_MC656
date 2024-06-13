e9cdbb955c886572b08f012142e539e38df8f94a
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-13 15:04:13 -0300

    feat: SchoolController store method

---
+++ backend/src/controllers/SchoolController.js
@@ -0,0 +1,31 @@
+const School = require("../models/School");
+const User = require("../models/User");
+
+class SchoolController {
+  static #validateSchoolData(schoolData) {
+    const { name, manager } = schoolData;
+    if (!name || !manager) {
+      return { ok: "Invalid data" };
+    }
+    return { ok: "Valid data" };
+  }
+
+  static async store(req, res) {
+    const validation = SchoolController.#validateSchoolData(req.body);
+    if (validation.ok !== "Valid data") {
+      return res.status(400).json(validation);
+    }
+
+    const { manager } = req.body;
+    const managerExists = await User.findUser({ email: manager });
+    if (managerExists.length <= 0) {
+      console.log("rip");
+      return res.status(400).json({ ok: "Manager doesn't exist" });
+    }
+
+    const insertionResult = await School.createSchool(req.body);
+    return res.status(200).json({ ok: "School created" });
+  }
+}
+
+module.exports = SchoolController;

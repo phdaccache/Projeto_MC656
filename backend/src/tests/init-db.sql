113044bcf847f26900241a600697921f7a0d5d5e
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-15 16:46:50 -0300

    feat: OlympiadUsers table

--- backend/src/tests/init-db.sql
+++ backend/src/tests/init-db.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS SchoolUsers;
+DROP TABLE IF EXISTS OlympiadUsers;
 DROP TABLE IF EXISTS Olympiad;
 DROP TABLE IF EXISTS School;
 DROP TABLE IF EXISTS Users;
@@ -46,6 +47,19 @@ CREATE TABLE SchoolUsers (
         REFERENCES Users (email)
 );
 
+CREATE TABLE OlympiadUsers (
+    olympiad VARCHAR,
+    school VARCHAR,
+    email VARCHAR,
+    interested BOOLEAN,
+
+    PRIMARY KEY (olympiad, school, email),
+    FOREIGN KEY (olympiad, school)
+        REFERENCES Olympiad (name, school),
+    FOREIGN KEY (email)
+        REFERENCES Users (email)
+);
+
 -- Default user to manage the schools
 INSERT INTO Users (name, email)
             VALUES ('Manager', 'schoolmanager@gmail.com');
@@ -53,3 +67,7 @@ INSERT INTO Users (name, email)
 -- Default school
 INSERT INTO School (name, manager)
             VALUES ('DefaultSchool', 'schoolmanager@gmail.com');
+
+-- Default olympiad
+INSERT INTO Olympiad (name, school, date_start, date_end, description)
+            VALUES ('DefaultOlympiad', 'DefaultSchool', '2024-01-01', '2024-01-02', 'DefaultOlympiadDescription');

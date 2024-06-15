02279fe7c16dd1e4798b04c884549175eb62746d
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-15 16:48:50 -0300

    feat: OlympiadUsers show & update interest

---
+++ backend/src/models/OlympiadUsers.js
@@ -0,0 +1,48 @@
+const DbClient = require("../lib/dbConnection");
+
+class OlympiadUsersModel {
+  static async hasSignedUp(olympiadUserData) {
+    const databaseConnection = DbClient.getInstance();
+
+    const { olympiad, school, email } = olympiadUserData;
+
+    const queryMessage = `
+        SELECT *
+        FROM OlympiadUsers
+        WHERE olympiad = '${olympiad}' AND school = '${school}' AND email = '${email}';`;
+    const queryResult = await databaseConnection.query(queryMessage);
+
+    return queryResult.rows;
+  }
+
+  static async showInterest(olympiadUserData) {
+    const databaseConnection = DbClient.getInstance();
+
+    const { olympiad, school, email, interested } = olympiadUserData;
+
+    const queryMessage = `
+      INSERT INTO OlympiadUsers (olympiad, school, email, interested)
+      VALUES ('${olympiad}', '${school}', '${email}', '${interested}');
+      `;
+
+    const queryResult = await databaseConnection.query(queryMessage);
+    return queryResult.rows;
+  }
+
+  static async updateInterest(olympiadUserData) {
+    const databaseConnection = DbClient.getInstance();
+
+    const { olympiad, school, email, interested } = olympiadUserData;
+
+    const queryMessage = `
+      UPDATE OlympiadUsers
+      SET interested = '${interested}'
+      WHERE olympiad = '${olympiad}' AND school = '${school}' AND email = '${email}';
+      `;
+
+    const queryResult = await databaseConnection.query(queryMessage);
+    return queryResult.rows;
+  }
+}
+
+module.exports = OlympiadUsersModel;

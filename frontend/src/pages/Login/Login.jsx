1bc57e66ee3e6cb6d449ada63e9fb296c393524a
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-06-17 14:10:15 -0300

    feat: update login page layout

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Login.css"
 
 export default function Login() {
     return (
-        <div className="login">
-            <p> Hello, World! </p>
+        <div className="root-div-flex-row flex-center-vh">
+            <div className="login-container">
+                <h2>Bem-Vindo ao Sistema!</h2>
+                <h3>Insira suas credenciais</h3>
+                <form className="login-form">
+                    <input type="text" placeholder="E-mail" />
+                    <input type="password" placeholder="Senha" />
+                    <div className="signup-forgot-links">
+                        <Link to={"/signup"}>Não possui uma conta?</Link>
+                        {/* <Link to={"/forgot"}>Esqueceu a senha?</Link> */}
+                    </div>
+                    <Link to="/"><button type="submit">Login</button></Link>
+                </form>
+            </div>
         </div>
     );
 };

fe7d671f75ae96b4c92066e65d5bf93dba66c157
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-06-17 14:49:43 -0300

    style: fix indentation

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
-import "./Login.css"
+import "./Login.css";
 
 export default function Login() {
-    return (
-        <div className="login-signup-background root-div-flex-row flex-center-vh">
-            <div className="login-container">
-                <h2>Bem-Vindo ao Sistema!</h2>
-                <h3>Insira suas credenciais</h3>
-                <form className="login-form">
-                    <input type="text" placeholder="E-mail" />
-                    <input type="password" placeholder="Senha" />
-                    <div className="signup-forgot-links">
-                        <Link to={"/signup"}>Não possui uma conta?</Link>
-                        {/* <Link to={"/forgot"}>Esqueceu a senha?</Link> */}
-                    </div>
-                    <Link to="/"><button type="submit">Login</button></Link>
-                </form>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="login-signup-background root-div-flex-row flex-center-vh">
+      <div className="login-container">
+        <h2>Bem-Vindo ao Sistema!</h2>
+        <h3>Insira suas credenciais</h3>
+        <form className="login-form">
+          <input type="text" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <div className="signup-forgot-links">
+            <Link to={"/signup"}>Não possui uma conta?</Link>
+            {/* <Link to={"/forgot"}>Esqueceu a senha?</Link> */}
+          </div>
+          <Link to="/">
+            <button type="submit">Login</button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}

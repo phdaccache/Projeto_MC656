2a67c3ed40ec4216e705959ed48491fd70553190
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-06-17 12:28:54 -0300

    feat: add login and signup pages
    This commit also includes routes for both pages.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
 import "./index.css";
 import App from "./App";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,9 @@
+import "./SignUp.css"
+
+export default function SignUp() {
+    return (
+        <div className="signup">
+            <p> Hello, World! </p>
+        </div>
+    );
+};

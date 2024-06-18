495a0cba23195c9e7715c5e21a2524d8f4e05881
andrac-23 <128061481+andrac-23@users.noreply.github.com>
2024-06-17 22:15:29 -0300

    feat: add final sidebar structure

--- frontend/src/pages/MainSidebar/MainSidebar.jsx
+++ frontend/src/pages/MainSidebar/MainSidebar.jsx
@@ -1,9 +1,69 @@
+import { React, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import './MainSidebar.css';
 
 export default function MainSidebar() {
+  // State to control sidebar collapse
+  const [sidebar, setSidebar] = useState(true);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  // State to control logout button visibility (arrow icon)
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const handleLogout = () => {
+    setShowConfirmation(!showConfirmation);
+  }
+
+  // @TODO Synchronize user profile picture and name with backend
+
+  // @TODO Add logout confirmation and functionality
+
   return (
-    <nav className="main-sidebar">
-      <h2>Sidebar</h2>
-    </nav>
+    <div className="root-div-flex-row">
+      <nav className={`sidebar-style ${sidebar ? "main-sidebar" : "main-sidebar-collapsed"}`}>
+        <div className='toggle-sidebar' onClick={showSidebar}>
+          <i className ={sidebar ? "fa-solid fa-chevron-left" : "fa-solid fa-chevron-right"}></i>
+        </div>
+        <div className='sidebar-logo'>
+          <i class="fa-solid fa-medal"></i>
+          <h2 className='sidebar-text'>Olympics</h2>
+        </div>
+        <hr />
+        <ul className='nav-links'>
+          <li className='nav-link'>
+            <NavLink to={"/home"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+              <i className="fa-solid fa-house"></i>
+              <span className='sidebar-text'>Home</span>
+            </NavLink>
+          </li>
+          <li className='nav-link'>
+            <NavLink to={"/settings"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+              <i className="fa-solid fa-user"></i>
+              <span className='sidebar-text'>My Account</span>
+            </NavLink>
+          </li>
+          <li className='nav-link'>
+            <NavLink to={"/help"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+              <i className="fa-solid fa-circle-question"></i>
+              <span className='sidebar-text'>Help</span>
+            </NavLink>
+          </li>
+        </ul>
+        <hr />
+        <div className='sidebar-footer'>
+          <img
+            src='https://via.placeholder.com/150'
+            alt='profile'
+            className='profile-picture'
+          />
+          <p className='sidebar-text'>Aluno X</p>
+          <div className='sidebar-logout sidebar-text' onClick={handleLogout}>
+            {/* <i className={`fa-solid ${showConfirmation ? 'fa-caret-up' : 'fa-caret-down'}`}></i> */}
+            <i className={`fa-solid fa-caret-down arrow-icon ${showConfirmation ? "rotate-up" : ""}`}></i>
+          </div>
+          <div className={`logout-link-a ${showConfirmation ? "active" : ""}`}><p><Link to ="/login">Log Out</Link></p></div>
+        </div>
+      </nav>
+    </div>
   );
-}
+};

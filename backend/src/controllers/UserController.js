b269d96ff48888572de2608168225ed5847cbaaf
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-22 17:08:53 -0300

    fix: removes login from UserController

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -59,28 +59,6 @@ class UserController {
     const insertionResult = await User.createUser(req.body);
     return res.status(200).json({ ok: "User created" });
   }
-
-  static async login(req, res) {
-    const { email, password } = req.body;
-
-    const findPassword = await User.findPassword(req.body);
-
-    if (findPassword.length == 0) {
-      return res.status(400).json({ ok: "User doesnt exist" });
-    }
-
-    const dbPassword = findPassword[0].password;
-    const passwordMatch = await bcrypt.compare(password, dbPassword);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: "Authentication failed" });
-    }
-
-    const token = jwt.sign({ userEmail: email }, "your-secret-key", {
-      expiresIn: "1d",
-    });
-
-    res.status(200).json({ token });
-  }
 }
 
 module.exports = UserController;

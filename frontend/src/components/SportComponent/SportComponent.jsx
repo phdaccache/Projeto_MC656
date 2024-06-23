6d32024d4176c9336a255b5a68a28db4eb0d1e36
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-23 18:13:58 -0300

    feat: allow user to show interest on sports

--- frontend/src/components/SportComponent/SportComponent.jsx
+++ frontend/src/components/SportComponent/SportComponent.jsx
@@ -1,9 +1,21 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useContext } from "react";
 import "./SportComponent.css";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "../../instances/axios";
 
-export default function Sport({ name, players, duration, rules, description, backgroundColor }) {
+export default function Sport({
+  eventName,
+  name,
+  players,
+  participants,
+  duration,
+  rules,
+  description,
+  backgroundColor,
+}) {
   const [openSport, setOpenSport] = useState(false);
   const contentRef = useRef(null);
+  const { auth } = useContext(AuthContext);
 
   useEffect(() => {
     const content = contentRef.current;
@@ -14,19 +26,59 @@ export default function Sport({ name, players, duration, rules, description, bac
     }
   }, [openSport]); // Dependency on openSportIndex ensures this runs every time it changes
 
+  async function handleRegister() {
+    let userSchool = "";
+    try {
+      const response = await axios.get("/schoolusers", {
+        headers: {
+          authorization: auth,
+        },
+      });
+      userSchool = response.data.userSchoolList[0].school;
+    } catch (error) {
+      console.log(error);
+    }
+
+    const userEmail = localStorage.getItem("email");
+
+    try {
+      const response = await axios.post("/userolympiadsports", {
+        olympiad: eventName,
+        school: userSchool,
+        sport: name,
+        email: userEmail,
+        preference: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="sport-item">
       <div className="sport-header" onClick={() => setOpenSport(!openSport)}>
-        <h3>{name}</h3>
+        <h3>
+          {name} ({participants} cadastrados)
+        </h3>
         <span className="sport-icon">{openSport ? "-" : "+"}</span>
       </div>
       <div className="sport-content" ref={contentRef}>
         <div className="sport-content-container">
-          <p><strong>Jogadores:</strong> {players}</p>
-          <p><strong>Duração:</strong> {duration}</p>
-          <p><strong>Regras:</strong> {rules}</p>
-          <p><strong>Descrição:</strong> {description}</p>
-          <button className="register-button">Registrar/Gostar</button>
+          <p>
+            <strong>Jogadores:</strong> {players}
+          </p>
+          <p>
+            <strong>Duração:</strong> {duration}
+          </p>
+          <p>
+            <strong>Regras:</strong> {rules}
+          </p>
+          <p>
+            <strong>Descrição:</strong> {description}
+          </p>
+          <button className="register-button" onClick={handleRegister}>
+            Registrar/Gostar
+          </button>
         </div>
       </div>
     </div>

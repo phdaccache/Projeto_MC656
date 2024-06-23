eefe116d3be0b39b49c622d0e3b06df970d03b7c
Gabryel Rodrigues Alves da Silva <g235394@dac.unicamp.br>
2024-06-22 22:43:53 -0300

    feat: fetch user name from backend
    Co-authored-by: Gabryel <143141018+ogabryel@users.noreply.github.com>
    Co-authored-by: André <128061481+andrac-23@users.noreply.github.com>

--- frontend/src/pages/MainSidebar/MainSidebar.jsx
+++ frontend/src/pages/MainSidebar/MainSidebar.jsx
@@ -1,76 +1,127 @@
-import { React, useState } from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { React, useState, useContext, useEffect } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "../../instances/axios";
 
-import './MainSidebar.css';
+import "./MainSidebar.css";
 
 export default function MainSidebar() {
+  const { auth, logout } = useContext(AuthContext);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userDataResponse = await axios.get("/users/", {
+        headers: {
+          authorization: auth,
+        },
+      });
+
+      const userData = userDataResponse.data.userList[0];
+      setName(userData.name.split(" ")[0]);
+    };
+
+    fetchData();
+  }, []);
+
   // State to control sidebar collapse
   const [sidebar, setSidebar] = useState(true);
   const showSidebar = () => {
     setSidebar(!sidebar);
     setShowConfirmation(false);
-  }
+  };
 
   // State to control logout button visibility (arrow icon)
   const [showConfirmation, setShowConfirmation] = useState(false);
   const handleLogout = () => {
     setShowConfirmation(!showConfirmation);
-  }
-
-  // @TODO Synchronize user profile picture and name with backend
-
-  // @TODO Add logout confirmation and functionality
+  };
 
   return (
     <div className="main-sidebar-background root-div-flex-row">
-      <nav className={`sidebar-style ${sidebar ? "main-sidebar" : "main-sidebar-collapsed"}`}>
-        <div className='toggle-sidebar' onClick={showSidebar}>
-          <i className ={sidebar ? "fa-solid fa-chevron-left" : "fa-solid fa-chevron-right"}></i>
+      <nav
+        className={`sidebar-style ${
+          sidebar ? "main-sidebar" : "main-sidebar-collapsed"
+        }`}
+      >
+        <div className="toggle-sidebar" onClick={showSidebar}>
+          <i
+            className={
+              sidebar ? "fa-solid fa-chevron-left" : "fa-solid fa-chevron-right"
+            }
+          ></i>
         </div>
-        <div className='sidebar-logo'>
+        <div className="sidebar-logo">
           <i class="fa-solid fa-medal"></i>
-          <h2 className='sidebar-text'>Olympics</h2>
+          <h2 className="sidebar-text">Olympics</h2>
         </div>
         <hr />
-        <ul className='nav-links'>
-          <li className='nav-link'>
-            <NavLink to={"/home"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+        <ul className="nav-links">
+          <li className="nav-link">
+            <NavLink
+              to={"/home"}
+              className={({ isActive, isPending }) =>
+                isActive ? "active" : isPending ? "pending" : ""
+              }
+            >
               <i className="fa-solid fa-house"></i>
-              <span className='sidebar-text'>Home</span>
+              <span className="sidebar-text">Home</span>
             </NavLink>
           </li>
-          <li className='nav-link'>
-            <NavLink to={"/settings"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+          <li className="nav-link">
+            <NavLink
+              to={"/settings"}
+              className={({ isActive, isPending }) =>
+                isActive ? "active" : isPending ? "pending" : ""
+              }
+            >
               <i className="fa-solid fa-user"></i>
-              <span className='sidebar-text'>My Account</span>
+              <span className="sidebar-text">My Account</span>
             </NavLink>
           </li>
-          <li className='nav-link'>
-            <NavLink to={"/help"} className={({isActive, isPending}) => isActive ? "active" : isPending ? "pending" : ""}>
+          <li className="nav-link">
+            <NavLink
+              to={"/help"}
+              className={({ isActive, isPending }) =>
+                isActive ? "active" : isPending ? "pending" : ""
+              }
+            >
               <i className="fa-solid fa-circle-question"></i>
-              <span className='sidebar-text'>Help</span>
+              <span className="sidebar-text">Help</span>
             </NavLink>
           </li>
         </ul>
         <hr />
-        <div className='sidebar-footer'>
+        <div className="sidebar-footer">
           <img
-            src='https://via.placeholder.com/150'
-            alt='profile'
-            className='profile-picture'
+            src={`https://robohash.org/${localStorage.getItem(
+              "email"
+            )}?set=set4`}
+            alt="profile"
+            className="profile-picture"
           />
-          <p className='sidebar-text'>Aluno X</p>
-          <div className='sidebar-logout sidebar-text' onClick={handleLogout}>
+          <p className="sidebar-text">{name}</p>
+          <div className="sidebar-logout sidebar-text" onClick={handleLogout}>
             {/* <i className={`fa-solid ${showConfirmation ? 'fa-caret-up' : 'fa-caret-down'}`}></i> */}
-            <i className={`fa-solid fa-caret-down arrow-icon ${showConfirmation ? "rotate-up" : ""}`}></i>
+            <i
+              className={`fa-solid fa-caret-down arrow-icon ${
+                showConfirmation ? "rotate-up" : ""
+              }`}
+            ></i>
           </div>
-          <div className={`logout-link-a ${showConfirmation  ? "active" : ""}`}><p><Link to ="/login">Log Out</Link></p></div>
+          <div className={`logout-link-a ${showConfirmation ? "active" : ""}`}>
+            <p onClick={logout}>Log Out</p>
+          </div>
         </div>
       </nav>
-      <div className={sidebar ? "childApp-normal-sidebar" : "childApp-small-sidebar"}>
+      <div
+        className={
+          sidebar ? "childApp-normal-sidebar" : "childApp-small-sidebar"
+        }
+      >
         <header></header>
         <Outlet />
       </div>
     </div>
   );
-};
+}
